5c2e1188ca32bb6b5eecdf0ab439ecc45e15a49e
Nasim <nasim.programmer24.com>
2024-05-11 23:00:35 +0600

    now user can delete a assignment from mongodb and made it using delete method

--- index.js
+++ index.js
@@ -72,6 +72,14 @@ async function run() {
       res.send(result);
     });
 
+    // Delete a assignment from mongodb server
+    app.delete("/assignment/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await assignmentCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
